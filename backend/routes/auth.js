0688d0c3b356e70aa5d14c19cf0ae075775eaf7b
olha-yakymenko <o.yakymenko.450@studms.ug.edu.pl>
2024-12-09 11:11:52 +0100

    Pobieranie danych

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -28,6 +28,20 @@ router.post('/login', async (req, res) => {
     }
 });
 
+router.get('/user/:username', async (req, res) => {
+    const { username } = req.params;
+    
+    try {
+        const user = await User.findOne({ where: { username } });
 
+        if (!user) {
+            return res.status(404).json({ error: 'User not found' });
+        }
+
+        res.json(user);  
+    } catch (error) {
+        res.status(500).json({ error: 'Server error' });
+    }
+});
 
 module.exports = router;
