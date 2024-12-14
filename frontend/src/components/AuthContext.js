49b4158884c04b8b210c0f35fd026bcfc8db96ad
olha-yakymenko <o.yakymenko.450@studms.ug.edu.pl>
2024-12-14 22:27:43 +0100

    logowanie

---
+++ frontend/src/components/AuthContext.js
@@ -0,0 +1,80 @@
+
+import React, { createContext, useContext, useState, useEffect } from 'react';
+import axios from 'axios';
+import Cookies from 'js-cookie'; 
+import { useNavigate } from 'react-router-dom';
+const AuthContext = createContext();
+
+export const useAuth = () => {
+  return useContext(AuthContext); 
+};
+
+export const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const navigate = useNavigate();
+
+  const checkUser = (auth_token, username) => {
+    return axios.get(`http://localhost:5007/api/auth/user/${username}`, {
+      headers: { Authorization: `Bearer ${auth_token}` },
+      withCredentials: true, 
+    });
+  };
+
+  useEffect(() => {
+    const token = Cookies.get('token'); 
+    const username = Cookies.get('username'); 
+    if (token && username) {
+      checkUser(token, username)
+        .then((response) => {
+          setUser(response.data);
+        })
+        .catch(() => {
+          setUser(null); 
+        })
+        .finally(() => {
+          setLoading(false);
+        });
+    } else {
+      setLoading(false);
+    }
+  }, []);
+
+  const login = (username, password) => {
+    setLoading(true);
+    axios
+      .post('http://localhost:5000/api/auth/login', { username, password }, { withCredentials: true })
+      .then((response) => {
+        const token = response.data.token;
+        console.log('Token otrzymany z serwera:', token); 
+  
+        console.log('Przed zapisem w cookies:', { token, username });
+  
+        Cookies.set('token', token);
+        console.log('Token zapisany w cookies:', Cookies.get('auth_token'));  
+        Cookies.set('username', username);
+        console.log('Username zapisany w cookies:', Cookies.get('username'));
+  
+        return checkUser(token, username);
+      })
+      .then((response) => {
+        setUser(response.data); 
+        navigate('/'); 
+      })
+      .catch((error) => {
+        console.error('Login failed:', error); 
+      })
+      .finally(() => {
+        setLoading(false); 
+      });
+  };
+  
+  if (loading) {
+    return <div>Loading...</div>; 
+  }
+  return (
+    <AuthContext.Provider value={{ user, login}}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
