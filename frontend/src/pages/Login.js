0e69684ca26989210e7b31808b4c0ad471ce7291
olha-yakymenko <o.yakymenko.450@studms.ug.edu.pl>
2024-12-14 22:44:56 +0100

    strona logowania

---
+++ frontend/src/pages/Login.js
@@ -0,0 +1,54 @@
+
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { useAuth } from '../components/AuthContext';
+
+const Login = () => {
+  const [username, setUsername] = useState('');  
+  const [password, setPassword] = useState('');
+  const { login } = useAuth();
+  const navigate = useNavigate();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      login(username, password);
+    } catch (error) {
+      console.error('Login failed:', error.response?.data?.error || error.message);
+    }
+  };
+
+  return (
+    <div className="login-container">
+      <h2>Login</h2>
+      <form onSubmit={handleSubmit}>
+        <div className="form-group">
+          <label htmlFor="username">Username:</label>
+          <input
+            type="text"
+            id="username"
+            placeholder="Enter your username"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}  // Zmieniamy email na username
+            required
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="password">Password:</label>
+          <input
+            type="password"
+            id="password"
+            placeholder="Enter your password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          />
+        </div>
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
+};
+
+export default Login;
