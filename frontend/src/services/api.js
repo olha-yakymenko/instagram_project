2720d9427b8be701f85f276cbac73c22a12d5fce
olha-yakymenko <o.yakymenko.450@studms.ug.edu.pl>
2024-12-14 22:04:16 +0100

    autoryzacja przez token i cookies frontend

---
+++ frontend/src/services/api.js
@@ -0,0 +1,21 @@
+
+import axios from 'axios';
+import Cookies from 'js-cookie';
+
+const api = axios.create({
+    baseURL: 'http://localhost:5000/api',
+    withCredentials: true, 
+});
+
+api.interceptors.request.use((config) => {
+    const token = Cookies.get('auth_token'); 
+    console.log("TOKEN", token)
+    if (token) {
+        config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+}, (error) => {
+    return Promise.reject(error);
+});
+
+export default api;
