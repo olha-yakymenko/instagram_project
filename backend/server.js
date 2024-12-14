98809209a79a1ec511a7654eb957ade14a87eca3
olha-yakymenko <o.yakymenko.450@studms.ug.edu.pl>
2024-12-14 21:38:48 +0100

    server i zmiana cors

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,3 @@
-
-
 const express = require('express');
 const cors = require('cors');
 const { connectDB } = require('./models/index');
@@ -7,7 +5,23 @@ const authRoutes = require('./routes/auth');
 
 const app = express();
 
+const allowedOrigins = ['http://localhost:3002']; 
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      if (allowedOrigins.includes(origin) || !origin) {
+        callback(null, true);
+      } else {
+        callback(new Error('Not allowed by CORS'));
+      }
+    },
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    credentials: true,
+  })
+);
+  
 app.use(cors());
+
 app.use(express.json());
 
 app.use('/api/auth', authRoutes);
