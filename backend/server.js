a36d14b618a0a3181884447299ed76f44a3d8483
olha-yakymenko <o.yakymenko.450@studms.ug.edu.pl>
2024-12-15 21:25:45 +0100

    websocket

--- backend/server.js
+++ backend/server.js
@@ -2,25 +2,34 @@ const express = require('express');
 const cors = require('cors');
 const { connectDB } = require('./models/index');
 const authRoutes = require('./routes/auth');
-
+const socketIo = require('socket.io');
+const http = require('http');
 const app = express();
+const { router: messageRoutes, socketSetup } = require('./routes/messages');
+const server = http.createServer(app);
+const io = socketIo(server, {
+  cors: {
+    origin: 'http://localhost:3002',
+    methods: ['GET', 'POST'],
+    credentials: true,
+  }
+});
+socketSetup(io);
 
 const allowedOrigins = ['http://localhost:3002']; 
-
 app.use(
   cors({
     origin: function (origin, callback) {
-      if (allowedOrigins.includes(origin) || !origin) { 
+      if (allowedOrigins.includes(origin) || !origin) {
         callback(null, true);
       } else {
         callback(new Error('Not allowed by CORS'));
       }
     },
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    credentials: true,  
+    credentials: true,
   })
 );
-
   
 app.use(cors());
 
