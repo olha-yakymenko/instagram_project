9a09a8ac8237c8ac1540bb23497033d9be1a944e
olha-yakymenko <o.yakymenko.450@studms.ug.edu.pl>
2024-12-25 13:27:05 +0100

    context dla powiadomien

---
+++ frontend/src/components/NotificationContext.js
@@ -0,0 +1,40 @@
+
+import React, { createContext, useContext, useState } from 'react';
+
+const NotificationContext = createContext();
+
+export const useNotifications = () => {
+  const context = useContext(NotificationContext);
+  if (!context) {
+    throw new Error('useNotifications must be used within a NotificationProvider');
+  }
+  return context;
+};
+
+export const NotificationProvider = ({ children }) => {
+  const [notifications, setNotifications] = useState([]);
+
+  const addNotification = (notification, isMqtt = false) => {
+    setNotifications((prevNotifications) => [...prevNotifications, { ...notification, isMqtt }]);
+
+    if (isMqtt) {
+      setTimeout(() => {
+        setNotifications((prevNotifications) =>
+          prevNotifications.filter((n) => n.id !== notification.id)
+        );
+      }, 5000);
+    }
+  };
+
+  const removeNotification = (id) => {
+    setNotifications((prevNotifications) =>
+      prevNotifications.filter((n) => n.id !== id)
+    );
+  };
+
+  return (
+    <NotificationContext.Provider value={{ notifications, addNotification, removeNotification }}>
+      {children}
+    </NotificationContext.Provider>
+  );
+};
