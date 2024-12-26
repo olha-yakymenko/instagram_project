af7073890d4a00f573465220d0c3f399d95a547d
olha-yakymenko <o.yakymenko.450@studms.ug.edu.pl>
2024-12-26 16:09:44 +0100

    wyszukiwanie

---
+++ frontend/src/pages/Search.js
@@ -0,0 +1,94 @@
+
+import React, { useState } from 'react';
+import api from '../services/api'; 
+import Post from '../components/Post'; 
+import Cookies from 'js-cookie'; 
+
+import { useNavigate } from 'react-router-dom'; 
+
+const Search = () => {
+  const [posts, setPosts] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [username, setUsername] = useState('');
+  const [error, setError] = useState('');
+  const navigate = useNavigate(); 
+
+  const fetchPosts = async (username) => {
+    try {
+      setLoading(true);
+      setError('');
+      const token = Cookies.get('token');
+
+      if (!token) {
+        console.error('Brak tokenu lub nazwy użytkownika');
+        return;
+      }
+
+      const response = await api.get(`/search?username=${username}`, {
+        headers: { Authorization: `Bearer ${token}` },
+      });
+
+      if (response.data.length === 0) {
+        setError('Brak postów dla tego użytkownika');
+      } else {
+        setPosts(response.data);
+      }
+      setLoading(false);
+    } catch (error) {
+      setLoading(false);
+      setError(error.response?.status === 404 ? 'Brak użytkownika' : 'Błąd pobierania postów');
+      console.error('Error fetching posts:', error);
+    }
+  };
+
+  const handleSearch = (e) => {
+    e.preventDefault();
+    fetchPosts(username);
+  };
+
+  const handleProfileClick = (user) => {
+    navigate(`/profile/${user}`); 
+  };
+
+  return (
+    <div className="search">
+      <h2>Wyszukaj posty użytkownika</h2>
+      <form onSubmit={handleSearch}>
+        <input
+          type="text"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+          placeholder="Wprowadź nazwę użytkownika"
+        />
+        <button type="submit">Szukaj</button>
+      </form>
+
+      {loading ? (
+        <p>Ładowanie postów...</p>
+      ) : error ? (
+        <p>{error}</p>
+      ) : (
+        <div className="posts">
+          {posts.map((userPosts, index) => (
+            <div key={index}>
+              <h3>
+                <button onClick={() => handleProfileClick(userPosts.user)}>
+                  {userPosts.user}
+                </button>
+              </h3>
+              {userPosts.posts.length === 0 ? (
+                <p>Brak postów</p>
+              ) : (
+                userPosts.posts.map((post) => (
+                  <Post key={post.id} post={post} />
+                ))
+              )}
+            </div>
+          ))}
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Search;
