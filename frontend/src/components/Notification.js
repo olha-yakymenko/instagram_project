f509f2f6507e8ef99149337f3057b4272d2d1fe7
olha-yakymenko <o.yakymenko.450@studms.ug.edu.pl>
2024-12-29 18:19:43 +0100

    powidomienie

---
+++ frontend/src/components/Notification.js
@@ -0,0 +1,54 @@
+
+import React, { useState, useEffect } from 'react';
+import { useNotifications } from './NotificationContext';
+import api from '../services/api';
+import { useAuth } from './AuthContext';
+
+const Notifications = () => {
+  const { notifications: contextNotifications, removeNotification } = useNotifications();
+  const [dbNotifications, setDbNotifications] = useState([]); 
+  const { user } = useAuth();
+  if (user.id=='underfined'){
+    console.log("nieznaleziono uzytkownika")
+  }
+  useEffect(() => {    
+    const fetchNotificationsFromDb = async () => {
+      try {
+        const response = await api.get(`/notifications/${user.id}`);
+        setDbNotifications(response.data); 
+      } catch (error) {
+        console.error('Error fetching notifications from DB:', error);
+      }
+    };
+
+    fetchNotificationsFromDb();
+  }, [user.id]);
+
+  const allNotifications = [...dbNotifications, ...contextNotifications];
+
+  return (
+    <div className="notifications">
+      <h3>Notifications</h3>
+      {allNotifications.length > 0 ? (
+        allNotifications.map((notification) => (
+          <div
+            key={notification.id}
+            className={`notification ${notification.type}`}
+            style={{
+              animation: 'slideIn 1s', 
+            }}
+          >
+            <p>{notification.content}</p>
+            {!notification.isMqtt && (
+              <button onClick={() => removeNotification(notification.id)}>Remove</button>
+            )}
+          </div>
+        ))
+      ) : (
+        <p>No new notifications.</p>
+      )}
+    </div>
+  );
+};
+
+export default Notifications;
