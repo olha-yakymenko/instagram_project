2babf4c33a93101eb4223bb1ec82c04c4b028b42
olha-yakymenko <o.yakymenko.450@studms.ug.edu.pl>
2024-12-29 18:18:18 +0100

    lista powidomien

---
+++ frontend/src/components/NotificationList.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { useNotifications } from './NotificationContext';
+
+const NotificationList = () => {
+  const { notifications } = useNotifications();
+  return (
+    <div className="notification-container">
+      {notifications.map((notification) => {
+        return (
+          <div
+            key={notification.id}
+            className={`notification ${notification.type}`}
+          >
+            <p>{notification.contentText}</p>
+          </div>
+        )
+      })}
+    </div>
+  );
+};
+
+export default NotificationList;
